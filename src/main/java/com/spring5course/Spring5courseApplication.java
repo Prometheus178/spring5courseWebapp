3e88d55b4448005f0f35bcf6088f16355c659071
siriusonestar <siriusonestar@yandex.ru>
2019-04-19 19:43:05 +0300

    adding jpa example

---
+++ src/main/java/com/spring5course/Spring5courseApplication.java
@@ -0,0 +1,13 @@
+package com.spring5course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5courseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5courseApplication.class, args);
+    }
+
+}
